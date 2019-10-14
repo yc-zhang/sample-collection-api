6deba8eb18ca4ef3b329ab4f1d4ff081c8974d8f
yc-zhang <turalyon.zhangyc@gmail.com>
2019-10-14 17:15:44 +0800

    Init gradle application.

---
+++ src/main/java/sample/collection/api/SampleCollectionApiApplication.java
@@ -0,0 +1,13 @@
+package sample.collection.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCollectionApiApplication.class, args);
+	}
+
+}
